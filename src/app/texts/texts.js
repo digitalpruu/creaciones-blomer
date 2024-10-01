ef63b0597382f7e7ccf4d0187cf3c29e02eb7ef1
kevinpicon01 <digitalpruu@gmail.com>
2024-10-01 17:15:44 -0500

    Changed media, color, texts

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -1,36 +1,38 @@
-const title = "Titulo del emprendimiento";
-const email = "Email del emprendimiento";
+const title = "Marisol Diseño y Moda";
+const slogan = "El arte de vestir a la mujer";
+const email = "mabe3gomez@gmail.com";
 const mensajeWhatsApp = encodeURIComponent(
-    `¡Hola! Estoy muy interesado en tus productos de ${title} y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!`
+    `¡Hola! Estoy interesado en los productos de ${title} y me gustaría recibir más información. ¡Espero su respuesta!`
 );
-const numeroWhatsApp = "Numero del emprendimiento";
+const numeroWhatsApp = "3204321394";
 const textos = {
     index: {
-        title: title,
+        title: `${title}`,
+        slogan: `${slogan}`,
     },
     homeContent: {
-        title: title,
+        title: `${title}`,
     },
     about: {
-        description1: "Descripción del emprendimiento parrafo uno",
-        description2: "Descripcion del emprendimiento parrafo dos"
+        description1: "*Marisol Diseño y Moda* te ofrece ropa y uniformes con un estilo único y moderno, pensado para realzar la belleza femenina.",
+        description2: "En *Marisol Diseño y Moda*, nuestra misión es crear prendas que combinen arte y moda, brindando a las mujeres la confianza de lucir impecables. Cada prenda es confeccionada con la máxima atención al detalle y la calidad. Confía en nosotros para encontrar la ropa que mejor se adapte a tu estilo. ¡En *Marisol Diseño y Moda*, vestimos a la mujer con arte!",
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
-        email: email
+        email: email,
     },
     footer: {
-        slogan:{
-            p1: "slogan del emprendimiento parrafo uno",
-            p2: "slogan del emprendimiento parrafo dos"
+        slogan: {
+            p1: "Marisol Diseño y Moda",
+            p2: slogan,
         },
-        address: "Dirección del emprendimiento",
+        address: "",
         phone: numeroWhatsApp,
         email: email,
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
-        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+        facebook: "https://www.facebook.com/marisol.bernal.161?mibextid=ZbWKwL",
+        instagram: "https://www.instagram.com/marisol.bernal.161?igshid=MTBxNndkc2dzbDU3ZQ==",
+        tiktok: "https://www.tiktok.com/@marisol.bernal04?_t=8pyaPD1Fxaf&_r=1",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
     },
-
 };
 export default textos;
